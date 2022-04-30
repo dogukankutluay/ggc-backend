816a06432cda87e6d1ace939cad5ac71323cac49
yagizanil14 <asilinsaat14@hotmail.com>
2022-04-30 11:54:19 +0300

    aaaa
    aaaa

--- controller/auth.js
+++ controller/auth.js
@@ -38,14 +38,88 @@ const register = asyncHandler(async (req, res, next) => {
     await user.save();
     const confirmEmailUrl = `https://ggc-coin-git-main-bekir-akok.vercel.app/auth/success?token=${confirmEmailToken}`;
     const emailTemplate = `
-          <h3>E-Mail'inizi onaylayınız</h3>
-          <p>Bu  <a href="${confirmEmailUrl}" target='_blank'>link</a> ile E-maili'nizi onaylayabilirsiniz.</p>
-          <p>Onay süresi 3 gündür.</p>
+    <!doctype html>
+    <html lang="en-US">
+    
+    <head>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+        <title>Email Verification</title>
+        <meta name="description" content="Email Verification">
+        <style type="text/css">
+            a:hover {text-decoration: underline !important;}
+        </style>
+    </head>
+    
+    <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+        <!--100% body table-->
+        <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+            style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+            <tr>
+                <td>
+                    <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                        align="center" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td style="height:80px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align:center;">
+                              <a href="https://ggcm.io" title="logo" target="_blank">
+                                <img width="60" height="60" src="https://i.hizliresim.com/5q96wbc.png" title="logo" alt="logo">
+                              </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="height:20px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                    style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                    <tr>
+                                        <td style="height:40px;">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding:0 35px;">
+                                            <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Email Verification Link</h1>
+                                            <span
+                                                style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                            <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                                                Here is the confirmation link for GGC.
+                                                In order to verify your email, click the link down below.
+                                            </p>
+                                            <a href="${confirmEmailUrl}"
+                                                style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Verify Email</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="height:40px;">&nbsp;</td>
+                                    </tr>
+                                </table>
+                            </td>
+                        <tr>
+                            <td style="height:20px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align:center;">
+                                <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>www.ggcm.io</strong></p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="height:80px;">&nbsp;</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <!--/100% body table-->
+    </body>
+    
+    </html>
       `;
     const mailOptions = {
       from: process.env.SMTP_USER,
       to: user.email,
-      subject: 'E-mail Onaylama',
+      subject: 'E-mail Verification',
       html: emailTemplate,
     };
     await sendEmail({
@@ -97,14 +171,87 @@ const forgotPassword = asyncHandler(async (req, res, next) => {
     const code = makeId(6);
     const changePasswordUrl = `https://ggc-coin-git-main-bekir-akok.vercel.app/auth/reset?code=${code}`;
     const emailTemplate = `
-    <h3>Şifre değişikliği</h3>
-    <p>Bu  <a href="${changePasswordUrl}" target='_blank'>link</a> ile şifrenizi sıfırlayabilirsiniz.</p>
-    <p>Onay süresi 3 gündür.</p>
+    <!doctype html>
+    <html lang="en-US">
+    
+    <head>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+        <title>Email Verification</title>
+        <meta name="description" content="Email Verification">
+        <style type="text/css">
+            a:hover {text-decoration: underline !important;}
+        </style>
+    </head>
+    
+    <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+        <!--100% body table-->
+        <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+            style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+            <tr>
+                <td>
+                    <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                        align="center" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td style="height:80px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align:center;">
+                              <a href="https://ggcm.io" title="logo" target="_blank">
+                                <img width="60" height="60" src="https://i.hizliresim.com/5q96wbc.png" title="logo" alt="logo">
+                              </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="height:20px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                    style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                    <tr>
+                                        <td style="height:40px;">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding:0 35px;">
+                                            <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">You have requested to reset your password</h1>
+                                            <span
+                                                style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                            <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                                            We cannot simply send you your old password. A unique link to reset your password has been generated for you. To reset your password, click the following link and follow the instructions.
+                                            </p>
+                                            <a href="${changePasswordUrl}"
+                                                style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset Password</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="height:40px;">&nbsp;</td>
+                                    </tr>
+                                </table>
+                            </td>
+                        <tr>
+                            <td style="height:20px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align:center;">
+                                <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>www.ggcm.io</strong></p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="height:80px;">&nbsp;</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <!--/100% body table-->
+    </body>
+    
+    </html>
 `;
     const mailOptions = {
       from: process.env.SMTP_USER,
       to: fUser.email,
-      subject: 'Şifre değişikliği',
+      subject: 'Change Password',
       html: emailTemplate,
     };
     await sendEmail({
