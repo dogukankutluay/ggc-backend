6e3232f62cf242b4ef5ca86ee2f2ab69ad323401
Doğukan <dogu_kutluay_1999@hotmail.com>
2022-05-14 04:52:32 +0300

    ad

--- controller/payment.js
+++ controller/payment.js
@@ -1,12 +1,20 @@
-const asyncHandler = require("express-async-handler");
-const Payment = require("../models/Payment");
-const { successReturn, errorReturn } = require("../helpers/CustomReturn");
+const asyncHandler = require('express-async-handler');
+const Payment = require('../models/Payment');
+const { successReturn, errorReturn } = require('../helpers/CustomReturn');
 
 const getPaymentAddress = asyncHandler(async (req, res, next) => {
   const { user } = req;
   try {
     const payment = await Payment.find({ userId: user._id });
-    return successReturn(res, { payment });
+    let result = payment.map((pay, i) => {
+      if (pay.coinName == 'trc') {
+        pay.verified_payment = pay.verified_payment.filter(
+          ver => ver.tokenId != '_'
+        );
+      }
+      return pay;
+    });
+    return successReturn(res, { payment: result });
   } catch (error) {
     return errorReturn(res, {
       error: error || error.message,
